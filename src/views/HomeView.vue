c88c5b674a7d2fb97ae665cbcdc0b6f41ec03efd
CORP\hsanchor <hector.sancho-rodriguez.external@capgemini.com>
2024-05-29 14:12:03 +0200

    Primera redireccion desde home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,25 @@
-<template>
-
-    
-
-</template>
+<template></template>
 
 <script>
-
-
 export default {
-  
-  
-
   data() {
     return {
       
     }
   },
-
+  created() {
+    this.primeraRedireccion(this.correo)
+  },
   methods: {
-    
+    primeraRedireccion(correo) {
+      correo = localStorage.correo;
+
+        if (correo == null) {
+          this.$router.push({ name: 'login' })
+        } else {
+          this.$router.push({ name: 'admin' })
+        }
+    }
   },
 }
 </script>
